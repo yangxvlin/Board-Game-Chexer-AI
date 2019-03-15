49546724e0272cf8da1521920d2d5b94868ada12
yangxvlin <1768528843@qq.com>
2019-03-15 16:13:35 +1100

    gogogo

--- project1/State.py
+++ project1/State.py
@@ -9,7 +9,7 @@ Description:
 class State:
     INITIAL_TURN = 0
 
-    def __init__(self, playing_player, player_pieces=None, obstacles=None, turns=INITIAL_TURN):
+    def __init__(self, playing_player, player_pieces=None, obstacles=None):
         """ """
         # playing_player
         self.playing_player = playing_player
@@ -17,8 +17,6 @@ class State:
         self.player_pieces = player_pieces
         # [Hexa(), ...]
         self.obstacles = obstacles
-        # turn# [1, ..., 256] inclusive
-        self.turns = turns
 
     def get_next_state(self, action):
         # update next player to play
@@ -27,14 +25,4 @@ class State:
         # update player_piece position|action
         # TODO
 
-
-        # update turns if player 0 is now playing
-        next_state.turns = self.turns
-        next_state._update_turns()
-
         return next_state
-
-    # TODO include this operation in previous method to reduce function call overhead
-    def _update_turns(self):
-        if self.playing_player == Board.Board.N_PLAYER[0]:
-            self.turns += 1
