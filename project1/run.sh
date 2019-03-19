4f4a2d86481b5c611b82bcac2c9bdb06de54821b
yangxvlin <1768528843@qq.com>
2019-03-19 12:04:40 +1100

    commit

--- project1/run.sh
+++ project1/run.sh
@@ -1,7 +1,8 @@
 # Author: XuLin Yang 
 # Date: 2019-3-16 17:20:16
 
-# python file.py -flag < input.txt > output.txt
-python search.py ./input/sample1.json
+# python file.py -flag input.txt > output.txt
+# python search.py ./input/sample1.json
 # python search.py ./input/sample2.json
 # python search.py ./input/sample3.json
+python search.py ./input/sample4.json
