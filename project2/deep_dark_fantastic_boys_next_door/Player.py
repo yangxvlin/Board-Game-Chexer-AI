25f301d0c9cb48f64703dfd5f82fe773bf1638bd
yangxvlin <1768528843@qq.com>
2019-05-18 13:37:07 +1000

    fix minor errors

--- project2/deep_dark_fantastic_boys_next_door/Player.py
+++ project2/deep_dark_fantastic_boys_next_door/Player.py
@@ -147,7 +147,7 @@ class Player:
 
         self.states_history.append(next_state)
         self.states_counter[next_state.snap()] += 1
-        print("!!!!!!!!", self.states_counter[frozenset(next_state.snap())], next_state.snap())
+        print("!!!!!!!!", self.states_counter[next_state.snap()], next_state.snap())
 
     def choose_eval(self, index=0):
         """
